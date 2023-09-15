9a5598f2154d3677ff3ff855d96928c0e2882f56
yun6160 <devyun6160@gmail.com>
2023-09-16 03:03:12 +0900

    Feat: css 추가

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
+import Button from "./Button";
+import styles from "./App.module.css";
+
 function App() {
   return (
     <div>
-      <h1>Welecome back!!!</h1>
+      <h1 className={styles.title}>Welecome back!!!</h1>
+      <Button text={"Continue"} />
     </div>
   );
 }
