a4b9c61dfcd92073ac678a975059762a5b311456
yun6160 <devyun6160@gmail.com>
2023-09-21 23:37:40 +0900

    feat: react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import { useState, useEffect } from "react";
-import Movie from "./components/Movie";
+import Home from "./routes/Home";
+import Detail from "./routes/Detail";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 //영화 전체를 보여주지 않고
 //router를 render 한다
 //router는 URL을 보고있는 component고
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/movie",
+    element: <Detail />,
+  },
+]);
+
 function App() {
-  return null;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,10 +1,13 @@
 import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 
 const Movie = ({ coverImage, title, summary, genres }) => {
   return (
     <div>
       <img src={coverImage} alt={title} />
-      <h2>{title}</h2>
+      <h2>
+        <Link to={`movie`}>{title}</Link>
+      </h2>
       <h5>{summary}</h5>
       <ul>
         {genres.map((genre) => (
